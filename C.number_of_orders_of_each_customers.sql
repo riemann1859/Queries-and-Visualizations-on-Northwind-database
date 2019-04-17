46e74ee2243c17fe9949331b0fea69bb1da22c64
yunus <yunus.karabulut1@gmail.com>
2019-04-17 15:41:42 +0300

    query for Q3

--- C.number_of_orders_of_each_customers.sql
+++ C.number_of_orders_of_each_customers.sql
@@ -2,4 +2,5 @@ select c."CustomerID", count(o."OrderID") as number_of_orders
 from customers as c
 left join orders as o
 on c."CustomerID"=o."CustomerID"
-group by 1;
+group by 1
+order by number_of_orders desc;
