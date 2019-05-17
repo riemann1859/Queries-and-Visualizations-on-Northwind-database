a959888adc3c65baa69afb02ee1d3503d09810fc
yunus <yunus.karabulut1@gmail.com>
2019-05-17 16:51:25 +0300

    query for Q20

--- T.most_recent_order_for_each_employee.sql
+++ T.most_recent_order_for_each_employee.sql
@@ -1,3 +1,4 @@
+--in the output of this query, there may be more than one order corresponding to some employee 
 select "OrderID", "CustomerID", "EmployeeID", "OrderDate", "RequiredDate"
 from orders as o1
 where "OrderDate"=(
@@ -5,3 +6,20 @@ where "OrderDate"=(
                    from orders as o2
                    where o2."EmployeeID"=o1."EmployeeID"
                   )
+                  
+-- solution to above fault
+
+select "OrderID", "CustomerID", "EmployeeID", "OrderDate", "RequiredDate"
+from orders as o1
+where "OrderDate"=(
+                   select max("OrderDate")
+                   from orders as o2
+                   where o2."EmployeeID"=o1."EmployeeID"
+                  )
+	   and
+	   "OrderID"=(
+		          select max("OrderID")
+		          from orders as o2
+		          where o2."EmployeeID"=o1."EmployeeID" and o1."OrderDate"=o2."OrderDate"
+	             )
+order by 3
