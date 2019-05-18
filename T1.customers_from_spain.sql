55a9da725f2f73c92079527777f8143114ccc6bf
yunus <yunus.karabulut1@gmail.com>
2019-05-18 14:16:43 +0300

    query for Q21

--- T1.customers_from_spain.sql
+++ T1.customers_from_spain.sql
@@ -6,3 +6,10 @@ where "Country" = 'Spain' and
 	        from orders as o
 	        where o."CustomerID"=c."CustomerID"
 	       )
+	       
+--the same query can be written with 'in' predicate as follows:
+
+select "CustomerID", "CompanyName"
+from customers as c
+where "Country" = 'Spain' and
+    "CustomerID" in (select "CustomerID" from orders)
