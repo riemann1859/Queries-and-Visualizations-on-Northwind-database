2abc68e2c893d8c27a5b3425ed02cbafcc01ceb8
yunus <yunus.karabulut1@gmail.com>
2019-05-18 15:36:19 +0300

    query for Q18

--- R.customers_satisfying_some_strange_conditions.sql
+++ R.customers_satisfying_some_strange_conditions.sql
@@ -5,3 +5,16 @@ where "EmployeeID" in
 group by 1
 having count(distinct "EmployeeID")=
      (select count(*) from employees where "Country" = 'USA')
+     
+--Different approach
+
+select * from customers as c
+where not exists (
+                  select * from employees as e
+	              where "Country"='USA' and
+	                    not exists (
+						            select * from orders as o
+							        where o."CustomerID"=c."CustomerID" 
+							          and o."EmployeeID"=e."EmployeeID"
+							       )
+                 )
