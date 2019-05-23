76eb2fc19c27f3cf76022c8a983901a889928897
yunus <yunus.karabulut1@gmail.com>
2019-05-23 14:06:48 +0300

    query for Q30

--- T10.number_of_cities_per_country.sql
+++ T10.number_of_cities_per_country.sql
@@ -1,8 +1,8 @@
 select "Country", count(*) as numcities
 from (
-	    select   "Country","City" from customers
+      select   "Country","City" from customers
       union
       select   "Country","City" from employees
-	  ) as sub
+     ) as sub
 group by 1
 order by 1
