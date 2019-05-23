f34fa462b7be2b68094ed525a93780cea6277e29
yunus <yunus.karabulut1@gmail.com>
2019-05-23 14:10:03 +0300

    query for Q32

--- U3.orders_of_the_customer_with_maximum_number_orders.sql
+++ U3.orders_of_the_customer_with_maximum_number_orders.sql
@@ -1,17 +1,17 @@
 
-with order_count as	  (
-	                     select "CustomerID", count(*) as count_
+with order_count as   (
+	               select "CustomerID", count(*) as count_
                        from orders
                        group by 1
                        order by 2 desc
-	                   )
+	              )
 select *
 from orders
 where "CustomerID"=(
 	                select "CustomerID"
 	                from order_count
 	                where count_=(
-								                select max(count_) 
+		                      select max(count_) 
 	                              from order_count
 	                              )
-	                )
+	           )
