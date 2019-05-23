8c7e753035d78ccc7cd9b5585b52cd3adf592075
yunus <yunus.karabulut1@gmail.com>
2019-05-23 14:17:33 +0300

    query for Q35

---
+++ U5.countries_with_customers_without_employees.sql
@@ -0,0 +1,21 @@
+
+select distinct c."Country"
+from customers as c
+where c."Country" not in (
+                          select e."Country"
+                          from employees  as e
+                          )
+order by 1
+
+--solution with exists predicate
+
+select distinct c."Country"
+from customers as c
+where not exists (
+                          select e."Country"
+                          from employees  as e
+	                        where e."Country"=c."Country"
+                  )
+order by 1
+
+
